6263306c0b984cae4d9fc1a5fb5758518d515c70
Xav18 <xavier.p.davis18@gmail.com>
2023-05-26 14:16:39 +0200

    imporved logging

--- odk2gn/contrib/flore_proritaire/src/odk_flore_prioritaire/main.py
+++ odk2gn/contrib/flore_proritaire/src/odk_flore_prioritaire/main.py
@@ -3,6 +3,9 @@ from odk_flore_prioritaire.odk_methods import update_priority_flora_db, write_fi
 from odk2gn.main import synchronize, upgrade_odk_form
 from odk2gn.odk_api import update_form_attachment
 from geonature.app import create_app
+import logging
+
+log = logging.getLogger("app")
 
 
 @synchronize.command(name="flore-prio")
@@ -11,14 +14,19 @@ from geonature.app import create_app
 def synchronize(project_id, form_id):
     app = create_app()
     with app.app_context() as app_ctx:
+        log.info(f"--- Start synchro for priority flora ---")
         update_priority_flora_db(project_id=project_id, form_id=form_id)
+
 
 
 @upgrade_odk_form.command(name="flore-prio")
 @click.option("--project_id", required=True, type=int)
 @click.option("--form_id", required=True, type=str)
 def upgrade_odk_form(project_id, form_id):
+    log.info("--- Start upgrade form for priority flora ---")
     app = create_app()
     with app.app_context() as app_ctx:
+        
         files = write_files()
         update_form_attachment(project_id, form_id, files)
+    log.info("--- Done ---")
