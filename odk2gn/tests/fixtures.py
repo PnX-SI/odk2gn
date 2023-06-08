4ac7df746a88a12e57dbb429f048b8a7a19cb281
Xav18 <xavier.p.davis18@gmail.com>
2023-06-08 09:04:24 +0200

    mise en place de quelque tests

--- odk2gn/tests/fixtures.py
+++ odk2gn/tests/fixtures.py
@@ -1,12 +1,36 @@
 import pytest
+import uuid
 
+from odk2gn.contrib.flore_proritaire.src.odk_flore_prioritaire.odk_methods import to_wkt
 from geonature.utils.env import db
 from geonature import create_app
 from geonature.core.gn_meta.models import (
-    TDatasets,
+    TDatasets
 )
+from geonature.core.gn_commons.models import TModules
 
+from pypnusershub.db.models import UserList, User
+from pypnnomenclature.models import TNomenclatures, BibNomenclaturesTypes
+from geonature.core.gn_monitoring.models import TBaseSites, corSiteModule
+from gn_module_monitoring.monitoring.models import (
+    TMonitoringModules, TMonitoringSites
+)
+from apptax.taxonomie.models import BibListes, CorNomListe, Taxref, BibNoms
 from utils_flask_sqla.tests.utils import JSONClient
+
+point = {
+    "geometry": {
+    "type": "Point",
+    "coordinates": [6.0535113, 44.5754145]
+  }
+}
+
+
+
+@pytest.fixture(scope='function')
+def taxon():
+    taxon = (815096, 'Homo sapiens Linnaeus, 1758','Homme moderne, Homme de Cro-Magnon')
+    return taxon
 
 
 @pytest.fixture(scope="session", autouse=True)
@@ -37,7 +61,7 @@ def _session(app):
 
 @pytest.fixture()
 def submissions():
-    return ["Buid a fake list of submissions", "TODO"]
+    return [{'sub_id': 1, 'taxon': 'Homo sapiens'}, {'sub_id' : 22, 'taxon' : 'Nardus stricta'}]
 
 
 @pytest.fixture(scope="function")
@@ -46,3 +70,57 @@ def datasets():
         TDatasets(id_dataset=1, dataset_name="ds1"),
         TDatasets(id_dataset=2, dataset_name="ds2"),
     ]
+
+@pytest.fixture(scope="function")
+def module():
+    with db.session.begin_nested():
+        new_module = TMonitoringModules(
+            module_code="MODULE_1",
+            module_label="module_1",
+            module_path="module_1",
+            active_frontend=True,
+            active_backend=False,
+        )
+        db.session.add(new_module)
+    return new_module
+
+@pytest.fixture(scope='function')
+def header():
+    return "column1", "column2"
+
+@pytest.fixture(scope='function')
+def data():
+    return ('1' +'2'), ('3','4')
+
+@pytest.fixture(scope='function')
+def nomenclature():
+    with db.session.begin_nested():
+
+        type_nom = BibNomenclaturesTypes(
+        mneumonique = 'TEST',
+        label_default = 'test', 
+        label_fr = 'Test'
+        )
+
+        nomenclature = TNomenclatures(
+            id_type = type_nom.id_nomenclature,
+            cd_nomenclature = 'test',
+            label_default = 'test', 
+            label_fr = 'Test',
+            active = True
+        )
+        db.session.add(type_nom, nomenclature)        
+    return nomenclature
+
+@pytest.fixture(scope='function')
+def site():
+    with db.session.begin_nested():
+        test_site=TBaseSites(
+        base_site_name = "test_site", 
+        geom = to_wkt(point['geometry']),
+            
+        )
+        
+        db.session.add(test_site)        
+    return test_site
+
--- odk2gn/tests/test_all.py
+++ odk2gn/tests/test_all.py
@@ -1,21 +1,23 @@
 import pytest
 
-from odk2gn.tests.fixtures import app, submissions, _session, datasets
-from odk2gn.gn2_utils import get_jdd_list
+from odk2gn.tests.fixtures import app, submissions, _session, datasets, header, data, taxon, module, point
+#from odk2gn.tests.fixtures import site
+from odk2gn.gn2_utils import get_jdd_list, to_csv, get_site_list, get_taxon_list
+from odk2gn.contrib.flore_proritaire.src.odk_flore_prioritaire.odk_methods import to_wkt
+#from odk2gn.odk_api import dummy
 
-# from odk2gn.odk_api import dummy
 
 
 @pytest.mark.usefixtures()
 class TestCommand:
-    def test_synchronize_monitoring(self, mocker, submissions):
+  def test_synchronize_monitoring(self, mocker, submissions):
         from odk2gn.main import dummy
 
         mocker.patch("odk2gn.main.get_submissions", return_value=submissions)
 
         # la vrai fonction a "mocker" est synchronize_monitoring et non 'dummy'
         submissions = dummy()
-        assert True
+        assert isinstance(submissions, list)
 
 
 @pytest.mark.usefixtures()
@@ -23,3 +25,17 @@ class TestUtilsFunctions:
     def test_get_jdd_list1(self, datasets):
         formated_ds = get_jdd_list(datasets)
         assert formated_ds == [[1, "ds1"], [2, "ds2"]]
+    
+    def test_to_csv1(self, header, data):
+        csv = to_csv(header, data) 
+        assert csv == "column1,column2"+'\n'+'1,2'+'\n'+'3,4'
+    
+    def test_get_taxon_list1(self, taxon):
+        taxons = get_taxon_list(100)
+        assert taxon in taxons
+
+    """def test_get_site_list1(self, site, module):
+        sites = module.id_module
+        assert site in sites"""
+    
+
