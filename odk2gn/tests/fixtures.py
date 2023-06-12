03c77ff12c4247747488283931830cc4f05f8aee
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2023-06-12 14:04:22 +0200

    add pyproject + black + temporar_transaction

--- odk2gn/tests/fixtures.py
+++ odk2gn/tests/fixtures.py
@@ -4,9 +4,7 @@ import uuid
 from odk2gn.contrib.flore_proritaire.src.odk_flore_prioritaire.odk_methods import to_wkt
 from geonature.utils.env import db
 from geonature import create_app
-from geonature.core.gn_meta.models import (
-    TDatasets
-)
+from geonature.core.gn_meta.models import TDatasets
 from sqlalchemy.event import listen, remove
 from geonature.core.gn_commons.models import TModules
 
@@ -14,75 +12,64 @@ from pypnusershub.db.models import UserList, User
 from pypnnomenclature.models import TNomenclatures, BibNomenclaturesTypes
 from geonature.core.gn_monitoring.models import TBaseSites, corSiteModule
 from gn_module_monitoring.monitoring.models import (
-    TMonitoringModules, TMonitoringSites, TMonitoringSitesGroups
+    TMonitoringModules,
+    TMonitoringSites,
+    TMonitoringSitesGroups,
 )
 from apptax.taxonomie.models import BibListes, CorNomListe, Taxref, BibNoms
 from utils_flask_sqla.tests.utils import JSONClient
 
-point = {
-    "geometry": {
-    "type": "Point",
-    "coordinates": [6.0535113, 44.5754145]
-  }
-}
+point = {"geometry": {"type": "Point", "coordinates": [6.0535113, 44.5754145]}}
 
 type_nom = BibNomenclaturesTypes(
-            mnemonique = 'TEST',
-            label_default = 'test', 
-            label_fr = 'Test'
-        )
+    mnemonique="TEST", label_default="test", label_fr="Test"
+)
 
 
+@pytest.fixture(scope="function")
+def test():
+    with db.session.begin_nested():
+        obs = User(identifiant="bidule", nom_role="bidule", prenom_role="bidule")
+        db.session.add(obs)
+        # db.session.commit()
+    return obs
 
 
+@pytest.fixture(scope="function")
+def bidule2():
+    with db.session.begin_nested():
+        obs_list = UserList(code_liste="test_list", nom_liste="test_liste")
+        db.session.add(obs_list)
+        db.session.commit()
+    return obs_list
 
 
 def create_nomenclature(nomenclature_type, cd_nomenclature, label_default, label_fr):
     nom = TNomenclatures(
-            id_type = nomenclature_type.id_type,
-            cd_nomenclature = cd_nomenclature,
-            label_default = label_default, 
-            label_fr = label_fr,
+        id_type=nomenclature_type.id_type,
+        cd_nomenclature=cd_nomenclature,
+        label_default=label_default,
+        label_fr=label_fr,
     )
     return nom
 
-@pytest.fixture(scope='function')
+
+@pytest.fixture(scope="function")
 def taxon():
-    taxon = (815096, 'Homo sapiens Linnaeus, 1758','Homme moderne, Homme de Cro-Magnon')
+    taxon = (
+        815096,
+        "Homo sapiens Linnaeus, 1758",
+        "Homme moderne, Homme de Cro-Magnon",
+    )
     return taxon
-
-
-@pytest.fixture(scope="session", autouse=True)
-def app():
-    app = create_app()
-    app.testing = True
-    app.test_client_class = JSONClient
-    app.config["SERVER_NAME"] = "test.geonature.fr"  # required by url_for
-
-    with app.app_context():
-        """
-        Note: This may seem redundant with 'temporary_transaction' fixture.
-        It is not as 'temporary_transaction' has a function scope.
-        This nested transaction is useful to rollback class-scoped fixtures.
-        Note: As we does not have a complex savepoint restart mechanism here,
-        fixtures must commit their database changes in a nested transaction
-        (i.e. in a with db.session.begin_nested() block).
-        """
-        # transaction = db.session.begin_nested()  # execute tests in a savepoint
-        yield app
-        # transaction.rollback()  # rollback all database changes
-
-
-@pytest.fixture
-def _session(app):
-    return db.session
-
-
 
 
 @pytest.fixture()
 def submissions():
-    return [{'sub_id': 1, 'taxon': 'Homo sapiens'}, {'sub_id' : 22, 'taxon' : 'Nardus stricta'}]
+    return [
+        {"sub_id": 1, "taxon": "Homo sapiens"},
+        {"sub_id": 22, "taxon": "Nardus stricta"},
+    ]
 
 
 @pytest.fixture(scope="function")
@@ -91,6 +78,7 @@ def datasets():
         TDatasets(id_dataset=1, dataset_name="ds1"),
         TDatasets(id_dataset=2, dataset_name="ds2"),
     ]
+
 
 @pytest.fixture(scope="function")
 def module():
@@ -105,47 +93,58 @@ def module():
         db.session.add(new_module)
     return new_module
 
-@pytest.fixture(scope='function')
+
+@pytest.fixture(scope="function")
 def header():
     return "column1", "column2"
 
-@pytest.fixture(scope='function')
-def data():
-    return ('1','2'), ('3','4')
 
-@pytest.fixture(scope='function')
+@pytest.fixture(scope="function")
+def data():
+    return ("1", "2"), ("3", "4")
+
+
+@pytest.fixture(scope="function")
 def nomenclature():
     with db.session.begin_nested():
         db.session.add(type_nom)
         db.session.flush()
-        nomenclature = create_nomenclature(type_nom, 'test', 'test', 'test')
-        nomenclature.active=True
-        db.session.add(nomenclature)      
+        nomenclature = create_nomenclature(type_nom, "test", "test", "test")
+        nomenclature.active = True
+        db.session.add(nomenclature)
     return nomenclature
 
-@pytest.fixture(scope='function')
-def site(module):
 
-    site_type = db.session.query(BibNomenclaturesTypes).filter(BibNomenclaturesTypes.mnemonique=='TYPE_SITE').one()
+@pytest.fixture(scope="function")
+def site(module):
+    site_type = (
+        db.session.query(BibNomenclaturesTypes)
+        .filter(BibNomenclaturesTypes.mnemonique == "TYPE_SITE")
+        .one()
+    )
 
     with db.session.begin_nested():
-
         # a créer en dehors de la fonction
-        nom = create_nomenclature(nomenclature_type=site_type,cd_nomenclature='test_site',label_default ='test_site', label_fr = 'Site Test')
+        nom = create_nomenclature(
+            nomenclature_type=site_type,
+            cd_nomenclature="test_site",
+            label_default="test_site",
+            label_fr="Site Test",
+        )
         nom.active = True
         db.session.add(nom)
         db.session.flush()
 
         mon_site = TMonitoringSites(
-            base_site_name = "test_site", 
-            geom = to_wkt(point['geometry']),
-            id_nomenclature_type_site = nom.id_nomenclature,
-            id_module = module.id_module,
+            base_site_name="test_site",
+            geom=to_wkt(point["geometry"]),
+            id_nomenclature_type_site=nom.id_nomenclature,
+            id_module=module.id_module,
         )
         mon_site.modules.append(module)
 
         module.sites.append(mon_site)
-        db.session.add(mon_site) 
+        db.session.add(mon_site)
 
     return mon_site
 
@@ -153,17 +152,10 @@ def site(module):
 @pytest.fixture(scope="function")
 def observers_and_list():
     # with db.session.begin_nested():
-    obs_list = UserList(
-    code_liste = 'test_list',
-    nom_liste = 'test_liste')
-    obs = User(
-        identifiant = 'test',
-        nom_role = 'User',
-        prenom_role = 'test'
-    )
+    obs_list = UserList(code_liste="test_list", nom_liste="test_liste")
+    obs = User(identifiant="test", nom_role="User", prenom_role="test")
     obs_list.users.append(obs)
     db.session.add(obs)
     db.session.add(obs_list)
     db.session.commit()
     return obs_list
-
--- odk2gn/tests/test_all.py
+++ odk2gn/tests/test_all.py
@@ -1,19 +1,40 @@
 import pytest
 
-from odk2gn.tests.fixtures import app, submissions, _session, datasets, header, data, taxon, module, point, module, nomenclature,  observers_and_list
+from odk2gn.tests.fixtures import (
+    submissions,
+    datasets,
+    header,
+    data,
+    taxon,
+    module,
+    point,
+    module,
+    nomenclature,
+    observers_and_list,
+    test,
+)
 from odk2gn.tests.fixtures import site
-from odk2gn.gn2_utils import get_jdd_list, to_csv, get_site_list, get_taxon_list, get_observer_list, get_nomenclature_data, get_ref_nomenclature_list
+from odk2gn.gn2_utils import (
+    get_jdd_list,
+    to_csv,
+    get_site_list,
+    get_taxon_list,
+    get_observer_list,
+    get_nomenclature_data,
+    get_ref_nomenclature_list,
+)
 from odk2gn.contrib.flore_proritaire.src.odk_flore_prioritaire.odk_methods import to_wkt
-#from odk2gn.odk_api import dummy
 
-from utils_flask_sqla.tests.fixtures import temporary_transaction
+# from odk2gn.odk_api import dummy
 
+from pypnusershub.db.models import UserList, User
 
+from geonature.utils.env import db
 
 
 @pytest.mark.usefixtures()
 class TestCommand:
-  def test_synchronize_monitoring(self, mocker, submissions):
+    def test_synchronize_monitoring(self, mocker, submissions):
         from odk2gn.main import dummy
 
         mocker.patch("odk2gn.main.get_submissions", return_value=submissions)
@@ -21,26 +42,26 @@ class TestCommand:
         # la vrai fonction a "mocker" est synchronize_monitoring et non 'dummy'
         submissions = dummy()
         assert isinstance(submissions, list)
-    
-  """def test_upgrade_odk_form_monitoring(self, mocker):
+
+    """def test_upgrade_odk_form_monitoring(self, mocker):
       assert """
 
 
-# @pytest.mark.usefixtures("")
+@pytest.mark.usefixtures("temporary_transaction")
 class TestUtilsFunctions:
     def test_get_jdd_list1(self, datasets):
         formated_ds = get_jdd_list(datasets)
         assert formated_ds == [[1, "ds1"], [2, "ds2"]]
-    
+
     def test_to_csv1(self, header, data):
-        csv = to_csv(header, data) 
-        assert csv == "column1,column2"+'\n'+'1,2'+'\n'+'3,4'
-    
+        csv = to_csv(header, data)
+        assert csv == "column1,column2" + "\n" + "1,2" + "\n" + "3,4"
+
     def test_get_taxon_list1(self, taxon):
         taxons = get_taxon_list(100)
         assert taxon in taxons
 
-    def test_get_observer_list1(self,observers_and_list):
+    def test_get_observer_list1(self, observers_and_list):
         observers = get_observer_list(observers_and_list.id_liste)
         user = observers_and_list.users[0]
         print("LAAAA", observers_and_list)
@@ -50,13 +71,16 @@ class TestUtilsFunctions:
     def test_get_site_list1(self, site, module):
         sites = get_site_list(module.id_module)
         site = site(module)
-        assert site in sites 
+        assert site in sites
 
-    
     def test_get_nomenclature_list1(self, nomenclature):
-        nomenclatures=get_ref_nomenclature_list(code_nomenclature_type='test')
+        nomenclatures = get_ref_nomenclature_list(code_nomenclature_type="test")
         assert nomenclature in nomenclatures
 
-    
-    
+    def test_bidule(self, test):
+        user = db.session.query(User).filter_by(identifiant="bidule").one()
+        print(user)
 
+    # def test_bidule2(test):
+    #     user = db.session.query(UserList).filter_by(identifiant="bidule").one()
+    #     print(user)
