0194ef4fe201aaf1a44afc50417563641de056e6
Xav18 <xavier.p.davis18@gmail.com>
2023-07-27 09:42:09 +0200

    monitoring modules tasks only

--- odk2gn/tasks.py
+++ odk2gn/tasks.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 from datetime import datetime, timedelta
 from pathlib import Path
@@ -6,7 +7,6 @@ from pathlib import Path
 from sqlalchemy import func
 from celery.utils.log import get_task_logger
 from celery.schedules import crontab
-from geonature.core.gn_commons.models import TModules
 from flask import current_app
 from geonature.utils.celery import celery_app
 from odk2gn.commands import synchronize_module, upgrade_module
@@ -23,34 +23,44 @@ def setup_periodic_tasks(sender, **kwargs):
     sender.add_periodic_task(
         crontab(minute="*/1"),
         synchronize_all_modules.s(),
-        name="synchronize_all",
+        name="synchronize_monitoring _modules",
     )
     sender.add_periodic_task(
         crontab(minute="*/1"),
         upgrade_all_forms.s(),
-        name="upgrade_all",
+        name="upgrade_monitoring_modules",
     )
 
 
 @celery_app.task(bind=True)
 def synchronize_all_modules(self):
     for form in TOdkForm.query.all():
-        logger.info(f"----Synchronizing module {form.module.module_code}----")
-        synchronize_module(
-            form.module.module_code,
-            project_id=form.odk_project_id,
-            form_id=form.odk_form_id,
-        )
-        logger.info(f"----{form.module.module_code} module synchronized----")
+        time.sleep(2)
+        if form.module.type == "monitoring_module":
+            logger.info(f"----Synchronizing module {form.module.module_code}----")
+            synchronize_module(
+                form.module.module_code,
+                project_id=form.odk_project_id,
+                form_id=form.odk_form_id,
+            )
+            logger.info(f"----{form.module.module_code} module synchronized----")
 
 
 @celery_app.task(bind=True)
 def upgrade_all_forms(self):
     for form in TOdkForm.query.all():
-        logger.info(f"----Upgrading form for module {form.module.module_code}----")
-        upgrade_module(
-            form.module.module_code,
-            project_id=form.odk_project_id,
-            form_id=form.odk_form_id,
-        )
-        logger.info(f"---{form.module.module_code} module upgraded----")
+        time.sleep(2)
+        if form.module.type == "monitoring_module":
+            logger.info(f"----Upgrading form for module {form.module.module_code}----")
+            upgrade_module(
+                form.module.module_code,
+                project_id=form.odk_project_id,
+                form_id=form.odk_form_id,
+                skip_jdd=False,
+                skip_nomenclatures=False,
+                skip_observers=False,
+                skip_sites=False,
+                skip_sites_groups=False,
+                skip_taxons=False,
+            )
+            logger.info(f"---{form.module.module_code} module upgraded----")
