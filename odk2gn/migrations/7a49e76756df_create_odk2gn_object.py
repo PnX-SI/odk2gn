91b3d79496d7c29be1ddb103c0324a3e95c67b9f
Xav18 <xavier.p.davis18@gmail.com>
2023-08-10 14:00:44 +0200

    updated migrations

---
+++ odk2gn/migrations/7a49e76756df_create_odk2gn_object.py
@@ -0,0 +1,39 @@
+"""create odk2gn object
+
+Revision ID: 7a49e76756df
+Revises: c1fa7f243102
+Create Date: 2023-08-10 10:31:03.903793
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = "7a49e76756df"
+down_revision = "c1fa7f243102"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.execute(
+        """
+        INSERT INTO gn_permissions.t_objects (code_object, description_object)
+        VALUES ('ODK2GN', 'Permissions sur odk2gn');
+        INSERT INTO gn_permissions.cor_object_module (id_object, id_module)
+        VALUES((SELECT id_object FROM gn_permissions.t_objects WHERE code_object = 'ODK2GN'), 
+                (SELECT id_module FROM gn_commons.t_modules WHERE module_code = 'ADMIN'));
+        """
+    )
+
+
+def downgrade():
+    op.execute(
+        """
+        DELETE FROM gn_permissions.cor_object_module 
+        WHERE id_object in (SELECT id_object FROM gn_permissions.t_objects WHERE code_object = 'ODK2GN');
+        DELETE FROM gn_permissions.t_objects
+        WHERE code_object = 'ODK2GN';
+        """
+    )
