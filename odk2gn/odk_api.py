3b2d16cbb9985006c07e0407ef15a1e73811d1e7
TheoLechemia <lechemia.theo@gmail.com>
2025-04-30 15:01:26 +0200

    add client.open() before each api interraction

--- odk2gn/odk_api.py
+++ odk2gn/odk_api.py
@@ -17,6 +17,7 @@ client = Client(config_path=odk2gn_config_file)
 
 
 def get_attachment(project_id, form_id, uuid_sub, media_name):
+    client.open()
     img = client.get(
         f"projects/{project_id}/forms/{form_id}/submissions/{uuid_sub}/attachments/{media_name}"
     )
@@ -27,43 +28,18 @@ def get_attachment(project_id, form_id, uuid_sub, media_name):
 
 
 def get_submissions(project_id, form_id):
+    client.open()
     # Creation client odk central
     form_data = None
-    with client:
-        form_data = client.submissions.get_table(
-            form_id=form_id,
-            project_id=project_id,
-            expand="*",
-            # TODO : try received or edited (but edited not actually support)
-            filter="__system/reviewState ne 'approved' and __system/reviewState ne 'hasIssues' and __system/reviewState ne 'rejected'",
-            # filter="__system/reviewState eq 'rejected'",
-        )
-        return form_data["value"]
-
-
-# def get_attachments(project_id, form_data):
-#     # #########################################
-#     #  Attachments
-#     # projects/1/forms/Sicen/submissions/{data['__id']}/attachments => Récupération de la liste des attachments pour une soumissions
-#     # projects/1/forms/Sicen/submissions/{data['__id']}/attachments/{att['name']} => Téléchargement de l'attachment pour la soumission
-#     for data in form_data:
-#         attachments_list = client.get(
-#             f"projects/{project_id}/forms//submissions/{data['__id']}/attachments"
-#         )
-#         print("Nombre de médias", {data["__id"]}, len(attachments_list.json()))
-#         print(attachments_list.json())
-#         for att in attachments_list.json():
-#             img = client.get(
-#                 f"projects/{project_id}/forms/Sicen/submissions/{data['__id']}/attachments/{att['name']}"
-#             )
-#             print(
-#                 "URL###################",
-#                 f"projects/{project_id}/forms/Sicen/submissions/{data['__id']}/attachments/{att['name']}",
-#             )
-#             with open(att["name"], "wb") as out_file:
-#                 out_file.write(img.content)
-
-#     assert response.status_code == 200
+    form_data = client.submissions.get_table(
+        form_id=form_id,
+        project_id=project_id,
+        expand="*",
+        # TODO : try received or edited (but edited not actually support)
+        # filter="__system/reviewState ne 'approved' and __system/reviewState ne 'hasIssues' and __system/reviewState ne 'rejected'",
+        # filter="__system/reviewState eq 'rejected'",
+    )
+    return form_data["value"]
 
 
 def update_review_state(project_id, form_id, submission_id, review_state):
@@ -126,9 +102,9 @@ def form_draft(project_id, xml_form_id):
     :param xml_form_id: nom du formulaire
     :type xml_form_id: str
     """
-    with client:
-        request = client.post(f"projects/{project_id}/forms/{xml_form_id}/draft")
-        assert request.status_code == 200
+    client.open()
+    request = client.post(f"projects/{project_id}/forms/{xml_form_id}/draft")
+    assert request.status_code == 200
 
 
 def upload_form_attachment(project_id, xml_form_id, file_name, data):
@@ -143,6 +119,7 @@ def upload_form_attachment(project_id, xml_form_id, file_name, data):
     :param data: csv data converti en chaine de caractères
     :type data: str
     """
+    client.open()
     response = client.post(
         f"{client.config.central.base_url}/v1/projects/{project_id}/forms/{xml_form_id}/draft/attachments/{file_name}",
         data=data.encode("utf-8", "strict"),
@@ -163,6 +140,7 @@ def publish_form(project_id, xml_form_id):
     :param xml_form_id: nom du formulaire
     :type xml_form_id: str
     """
+    client.open()
     version_number = datetime.now()
     response = client.post(
         f"projects/{project_id}/forms/{xml_form_id}/draft/publish?version={version_number}"
@@ -170,24 +148,20 @@ def publish_form(project_id, xml_form_id):
     assert response.status_code == 200
 
 
-def get_schema_fields(project_id, xml_form_id):
-    resp = client.get(f"projects/{project_id}/forms/{xml_form_id}/fields?odata=false")
-    return resp.json()
-
 
 class ODKSchema:
     def __init__(self, project_id, form_id):
         self.project_id = project_id
         self.form_id = form_id
         self.schema = self._get_schema_fields()
+        client.open()
 
     def _get_schema_fields(self):
-        with client:
-            resp = client.get(
-                f"projects/{self.project_id}/forms/{self.form_id}/fields?odata=false"
-            )
-            assert resp.status_code == 200
-            return resp.json()
+        resp = client.get(
+            f"projects/{self.project_id}/forms/{self.form_id}/fields?odata=false"
+        )
+        assert resp.status_code == 200
+        return resp.json()
 
     def get_field_info(self, field_name):
         try:
